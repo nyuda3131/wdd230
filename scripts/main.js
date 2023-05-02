9152974606806adcc6caba79324914b791bc882d
nyuda3131 <yuda06001@gmail.com>
2023-05-02 00:10:33 -0600

    W02 Assignment: Design Principles Document

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+document.querySelectorAll(".currentYear").forEach(function (element) {
+  element.textContent = new Date().getFullYear();
+});
+document.querySelectorAll(".lastModified").forEach(function (element) {
+  element.textContent = document.lastModified;
+});
