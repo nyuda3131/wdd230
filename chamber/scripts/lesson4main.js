4c552a6f8a581f60b2d031d0ded65da4092246c7
nyuda3131 <yuda06001@gmail.com>
2023-05-30 02:27:38 -0600

    W05 Assignment: Chamber Home Page Continued

--- chamber/scripts/lesson4main.js
+++ chamber/scripts/lesson4main.js
@@ -1,25 +1,64 @@
 function toggleMenu() {
-    const menuItems = document.querySelector(".menu-items");
-    menuItems.style.display = menuItems.style.display === "flex" ? "none" : "flex";
-  }
-  
-  document.querySelectorAll(".currentYear").forEach(function (element) {
-    element.textContent = new Date().getFullYear();
-  });
-  
-  document.querySelectorAll(".lastModified").forEach(function (element) {
-    element.textContent = document.lastModified;
-  });
+  const menuItems = document.querySelector(".menu-items");
+  menuItems.style.display =
+    menuItems.style.display === "flex" ? "none" : "flex";
+}
 
-  function getCurrentDate() {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const currentDate = new Date();
-    const day = days[currentDate.getDay()];
-    const month = months[currentDate.getMonth()];
-    const date = currentDate.getDate();
-    const year = currentDate.getFullYear();
-    return `${day}, ${date} ${month} ${year}`;
-  }
-  
-  document.getElementById("currentDate").textContent = getCurrentDate();
+document.querySelectorAll(".currentYear").forEach(function (element) {
+  element.textContent = new Date().getFullYear();
+});
+
+document.querySelectorAll(".lastModified").forEach(function (element) {
+  element.textContent = document.lastModified;
+});
+
+function getCurrentDate() {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const currentDate = new Date();
+  const day = days[currentDate.getDay()];
+  const month = months[currentDate.getMonth()];
+  const date = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  return `${day}, ${date} ${month} ${year}`;
+}
+
+document.getElementById("currentDate").textContent = getCurrentDate();
+
+let banner = document.createElement("div");
+banner.style.display = "none";
+banner.style.backgroundColor = "#708080";
+banner.style.color = "white";
+banner.style.padding = "10px";
+banner.style.textAlign = "center";
+banner.textContent =
+  "Come join us for the chamber meet and greet every Wednesday at 7:00 p.m.";
+document.body.insertBefore(banner, document.body.firstChild);
+
+let today = new Date();
+let dayOfWeek = today.getDay();
+
+if (dayOfWeek === 1 || dayOfWeek === 2) {
+  banner.style.display = "block";
+}
